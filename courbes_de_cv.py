7355866fa79c8a73daf4b581ec9eaec81dc05da3
Faiez Khalfallah <faiez.khalfallah@ensta-paris.fr>
2023-06-07 09:40:29 +0200

    code commente

--- courbes_de_cv.py
+++ courbes_de_cv.py
@@ -1,15 +1,19 @@
+#import the necessary libraries
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
+#data for the P1 fe
 data1 = np.loadtxt("C:/Users/khfz2/OneDrive/Bureau/Pro_docs/2A/PRe/FreeFEM/courbe_de_convergence_P1.txt")
 n_values_P1 = data1[:, 0]
 h1_norms_P1 = data1[:, 1]
 
+#data for the P2 fe
 data2 = np.loadtxt("C:/Users/khfz2/OneDrive/Bureau/Pro_docs/2A/PRe/FreeFEM/courbe_de_convergence_P2.txt")
 n_values_P2 = data2[:, 0]
 h1_norms_P2 = data2[:, 1]
 
+#data for the P3 fe
 data3 = np.loadtxt("C:/Users/khfz2/OneDrive/Bureau/Pro_docs/2A/PRe/FreeFEM/courbe_de_convergence_P3.txt")
 n_values_P3 = data3[:, 0]
 h1_norms_P3 = data3[:, 1]
@@ -26,6 +30,7 @@ plt.subplot(1,3,3)
 plt.loglog(n_values_P3, h1_norms_P3,'g', label="P3")
 plt.xlabel("P3 error")
 
+#compute the slope for each plot using a linear regression
 model = LinearRegression()
 model.fit(np.log(n_values_P1).reshape((-1, 1)),np.log(h1_norms_P1))
 print(print(f"slope for P1: {model.coef_}"))
@@ -33,7 +38,6 @@ model.fit(np.log(n_values_P2).reshape((-1, 1)),np.log(h1_norms_P2))
 print(print(f"slope for P2: {model.coef_}"))
 model.fit(np.log(n_values_P3).reshape((-1, 1)),np.log(h1_norms_P3))
 print(print(f"slope for P3: {model.coef_}"))
-
 
 # Display the plot
 plt.show()
