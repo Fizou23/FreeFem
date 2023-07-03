054ce1d89e02c443c9e1f13e7503a2cd718ac58d
Fizou23 <faiez.khalfallah@ensta-paris.fr>
2023-07-03 20:26:41 +0200

    heat_1D

---
+++ freefemfuncs/external.cpp
@@ -0,0 +1,14 @@
+//external functions.cpp
+
+#include <iostream>
+using namespace Fem2D;
+using namespace std;
+
+double xtox2(double x) {
+    return(x*x);
+}
+
+int main() {
+    cout<<"compilation ok!"<<endl;
+    return 0;
+}
